ae3d20b87d586f52afa8bf2da73197be8db57f8a
jingdian <461320432@qq.com>
2019-01-28 11:19:57 +0800

    初始化后台管理

---
+++ shop-admin/src/axios.js
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+let baseUrl = "http://122.112.244.157:8001/";
---
+++ shop-admin/src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "Index",
+
+        props: {
+
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
