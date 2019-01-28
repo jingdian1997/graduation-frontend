be7f7b3d6b6ae53cf58ce479504c8e5ffcc43b10
jingdian <461320432@qq.com>
2019-01-28 11:09:00 +0800

    初始化商城

---
+++ shop/src/axios.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+
+let baseUrl = 'http://122.112.244.157:8000/';
+
+//把整个项目的网络请求都写在这个文件中用export导出
---
+++ shop/src/components/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="index">
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Index",
+
+        props: {
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
