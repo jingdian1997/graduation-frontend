21e6f05ec0976820cd424c58ed8755c4863afdc3
jingdian <461320432@qq.com>
2019-02-15 10:49:46 +0800

    后台管理-更改依赖

--- shop-admin/src/axios.js
+++
@@ -1,3 +0,0 @@
-import axios from 'axios';
-
-let baseUrl = "http://122.112.244.157:8001/";
--- shop-admin/src/main.js
+++ shop-admin/src/main.js
@@ -2,14 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import iView from 'iview'; // 导入组件库
-import 'iview/dist/styles/iview.css'; // 导入样式
 
-Vue.use(iView);
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app');
+}).$mount('#app')
